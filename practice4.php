0b60f69d909075b04d90fb9f0f99234a30629663
payn873 <142923202+payn873@users.noreply.github.com>
2024-10-27 23:09:36 -0500

    Update practice4.php
    Fixed nested for loop to have <= instead of <

--- practice4.php
+++ practice4.php
@@ -8,7 +8,7 @@ echo "<table border = '1'>";
 
 for($i = 1; $i <= $_POST["size"]; $i++){
 	echo "<tr>";
-	for($j = 1; $j < $_POST["size"]; $j++){
+	for($j = 1; $j <= $_POST["size"]; $j++){
 		echo "<td>" . $i * $j. "</td>";
 	}
 	echo "</tr>";
